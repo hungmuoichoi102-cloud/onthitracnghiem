0bf014fbe43ac72574c623d5947bf0fa5f180a98
Choi <hungmuoichoi102@gmail.com>
2025-12-07 15:12:33 +0700

    updated dbconnect

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,12 @@
 <?php
-// $servername = "sql100.infinityfree.com";
-// $username = "if0_40531169";
-// $password = "Tanhung2602";
-// $dbname = "if0_40531169_on_trac_nghiem";
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "on_trac_nghiem";
+$servername = "sql100.infinityfree.com";
+$username = "if0_40531169";
+$password = "Tanhung2602";
+$dbname = "if0_40531169_on_trac_nghiem";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "on_trac_nghiem";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
